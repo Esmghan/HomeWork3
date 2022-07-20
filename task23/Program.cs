0473ecdc9c0fdef8139747538eb0eed7ffceafdf
Esmghan <11stv.ru@mail.ru>
2022-07-21 00:08:58 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Таблица кубов: ");
+for (int i = 0; i < number; i++) {
+    Console.Write($"{Math.Pow(i+1, 3)} ");
+}
