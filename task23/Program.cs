cd1c304b161e70afe8aaca750fcc0cadc4e035a7
Esmghan <11stv.ru@mail.ru>
2022-07-21 00:36:04 +0300

    task23 addition

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,7 +1,19 @@
-﻿Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
+// Console.Write("Таблица кубов: ");
+// for (int i = 0; i < number; i++) {
+//     Console.Write($"{Math.Pow(i+1, 3)} ");
+// }
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int [] cubes = new int[N];
 Console.Write("Таблица кубов: ");
-for (int i = 0; i < number; i++) {
-    Console.Write($"{Math.Pow(i+1, 3)} ");
+int i = 0;
+while(i < N){
+    cubes[i] = (i+1)*(i+1)*(i+1);
+    Console.Write($"{cubes[i]} ");
+    i++;
 }
