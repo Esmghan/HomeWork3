4658d44689e33259d04545574f2e58cd2bbfee4e
Esmghan <11stv.ru@mail.ru>
2022-07-21 10:33:53 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ 
+string FiveDigit = n.ToString();
+
+if (n>99999 || n<10000) {
+    Console.WriteLine("Число не соответствует условию задачи");}
+
+else if(FiveDigit[0] == FiveDigit[4] && FiveDigit[1] == FiveDigit[3]) {
+    Console.WriteLine ($"Числo {n} является палиндромом");}
+
+else {
+    Console.WriteLine ($"Число {n} - не палиндром");
+}
+
